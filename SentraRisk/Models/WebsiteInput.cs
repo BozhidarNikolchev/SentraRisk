cef9f80458b94f607a1394d5f46f6900448eab3c
BozhidarNikolchev <bozhidar650@gmail.com>
2026-07-01 20:51:00 +0200

    1st automation check

--- SentraRisk/Models/WebsiteInput.cs
+++ SentraRisk/Models/WebsiteInput.cs
@@ -2,6 +2,7 @@ namespace SentraRisk.Models
 {
     public class WebsiteInput
     {
+        public string WebsiteUrl { get; set; } = "";
         public bool UsesHttps { get; set; }
         public bool HasBackup { get; set; }
         public bool UsesOutdatedPlugins { get; set; }
---
+++ SentraRisk/Services/WebsiteScanner.cs
@@ -0,0 +1,30 @@
+using System.Net.Http;
+
+namespace SentraRisk.Services
+{
+    public class WebsiteScanner
+    {
+        public async Task<bool> CheckHttpsAsync(string website)
+        {
+            try
+            {
+                if (!website.StartsWith("http"))
+                {
+                    website = "https://" + website;
+                }
+
+                using var client = new HttpClient();
+
+                client.Timeout = TimeSpan.FromSeconds(10);
+
+                var response = await client.GetAsync(website);
+
+                return response.RequestMessage?.RequestUri?.Scheme == "https";
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
